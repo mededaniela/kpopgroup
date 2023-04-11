886fb11ba19125e2b546f2b286503ed093a92936
mededaniela <mededaniela99@gmail.com>
2023-04-11 18:52:05 +0300

    Added Dockerfile and thymeleaf templates

--- src/main/resources/static/js/dropdownlist.js
+++
@@ -1,9 +0,0 @@
-for (let key in countriesData) {
-  let option = document.createElement("option");
-  option.setAttribute('value', data[key]);
-
-  let optionText = document.createTextNode(key);
-  option.appendChild(optionText);
-
-  membersDropDown.appendChild(option);
-}
